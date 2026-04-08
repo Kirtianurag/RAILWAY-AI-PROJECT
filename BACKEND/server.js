a4da9b7ea85acd74e9be1821bcf3201c5255afcf
Kirtianurag <kirtianurag2003@gmail.com>
2026-04-08 20:38:10 +0530

    fix port for railway deployment

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -38,10 +38,10 @@ const startServer = async () => {
 
 
 
-    app.listen(5000, () => {
+    const PORT = process.env.PORT || 5000;
 
-      console.log("Server running on http://localhost:5000");
-
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
 
   } catch (error) {
