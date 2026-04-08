911f14f9161bd8ba7d16e74dfca50c2fd36c715c
Kirtianurag <kirtianurag2003@gmail.com>
2026-04-08 23:04:51 +0530

    fix railway health route

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -8,34 +8,38 @@ dotenv.config();
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// routes
 app.use("/api/auth", authRoutes);
 
-// test route
+/* health routes */
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
 
-// start server
+app.get("/api", (req, res) => {
+  res.send("API working");
+});
+
 const startServer = async () => {
   try {
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected");
 
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 8080;
 
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
 
   } catch (error) {
+
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
+
   }
 };
 
